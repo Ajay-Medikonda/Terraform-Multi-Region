e182f883484c27b9f021ba6427f03e9bd4d24564
Ajay-Medikonda <ajay.medikonda@yahoo.com>
2023-10-10 12:12:20 +0530

    Vpc perring updated

--- 7.peering.tf
+++ 7.peering.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc_peering_connection" "VPC_Peering_1_2" {
+  #provider          = aws.east-1  
   peer_owner_id = var.peer_owner_id
   peer_vpc_id   = aws_vpc.default-1.id
   vpc_id        = aws_vpc.default-2.id
@@ -18,16 +19,18 @@ resource "aws_vpc_peering_connection" "VPC_Peering_1_2" {
 }
 
 resource "aws_route" "aws_vpc_default-1" {
+  #provider          = aws.east-1
   route_table_id            = aws_route_table.terraform-public1.id
-  destination_cidr_block    = "10.0.0.0/16"
+  destination_cidr_block    = "192.0.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.VPC_Peering_1_2.id
   #depends_on                = [aws_route_table.terraform-public]
   #cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_route" "aws_vpc_default-2" {
+  #provider          = aws.east-1
   route_table_id            = aws_route_table.terraform-public2.id
-  destination_cidr_block    = "192.0.0.0/16"
+  destination_cidr_block    = "10.0.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.VPC_Peering_1_2.id
   #depends_on                = [aws_route_table.terraform-public]
   #cidr_block = "192.0.0.0/16"
